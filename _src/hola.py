3e36643c626fd9638ed74e2405c5be7858b1552d
Justin Burns <justin.burns@watech.wa.gov>
2016-04-27 20:10:43 +0000

    updating

--- _src/hola.py
+++ _src/hola.py
@@ -1,6 +1,7 @@
-import requests
-import botocore
+import boto3
 import logging
+import requests
+from datetime import datetime
 
 LOG = logging.getLogger()
 LOG.setLevel(logging.DEBUG)
@@ -8,7 +9,9 @@ LOG.setLevel(logging.DEBUG)
 r = requests.get("https://glassfrog.holacracy.org/api/v3/roles?api_key=e741d8d9f76972ae3e720971c976b626d54d214b")
 
 def handler(event, context):
-
+    s3 = boto3.resource('s3')
+    s3.Object('kappatest', datestring).put(Body=r.content)
+    
     LOG.debug(r.text)
     LOG.debug(event)
     return {'status': 'success'}
